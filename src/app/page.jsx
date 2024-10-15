2bcc752bb01eeaee96c27e73101db6a5f3b3a631
Vitor-Linaris <vitor.linaris@gmail.com>
2024-10-15 09:24:59 -0300

    feat: create title section component and your styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,10 @@
 import { ContainerGrid } from "@/components/container.component";
+import { TitleSection } from "@/components/title-section.component";
 
 export default function Home() {
   return (
     <ContainerGrid>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore culpa,
-        minima eveniet error fuga cupiditate aspernatur dolores, minus aut
-        molestias similique odio deserunt? Dignissimos soluta natus nemo
-        doloribus officiis tenetur?
-      </p>
+      <TitleSection title="Destaque em Coleções" subtitle="Coleções notáveis" />
     </ContainerGrid>
   );
 }
